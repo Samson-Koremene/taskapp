55e3fca5386419b7340aaba5049e138eaad77d21
Samson Koremene <106883706+Samson-Koremene@users.noreply.github.com>
2024-06-08 20:30:22 +0100

    pass

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./component/Header";
+import Todo from "./component/Todo";
+import "./App.css";
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Todo />
+    </div>
+  );
+}
+
+export default App;
